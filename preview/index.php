e6ba54cfc1c08678ff0d9cb6b63412d81a36627b
Markus Weisse <markus_harald.weisse@htl.rennweg.at>
2023-10-18 08:12:54 +0200

    preview v_1_0

---
+++ preview/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="styles.css" rel="stylesheet"/>
+	</head>
+	<body>
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span> 
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span> 
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+
+			<!-- vimeo -->
+			
+			<span><iframe src="https://www.youtube.com/embed/K3QDDlWmR9Q" width="400px" height="300px" title="W3Schools Free Online Web Tutorials"></iframe></span>
+	</body>
+</html>
